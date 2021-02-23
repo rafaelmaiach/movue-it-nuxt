abbfe896d993875288300265eadcc914c9e7c9e3
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-02-23 18:37:55 -0300

    chore: adds some configuration
    - Updates eslint config for tab and semicolon usage
    - Adds tailwind config file
    - Remove unused files and code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,29 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:nuxt/recommended'
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
-}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	extends: [
+		'@nuxtjs/eslint-config-typescript',
+		'plugin:nuxt/recommended',
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'comma-dangle': ['error', 'always-multiline'],
+		indent: ['error', 'tab'],
+		'no-tabs': 'off',
+		semi: ['error', 'always'],
+		'vue/html-indent': ['error', 'tab'],
+	},
+	overrides: [
+		{
+			files: ['*.vue'],
+			rules: {
+				indent: 'off',
+			},
+		},
+	],
+};
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+export default {
+	theme: {},
+	variants: {},
+	plugins: [],
+	purge: {
+		enabled: process.env.NODE_ENV === 'production',
+		content: [
+			'components/**/*.vue',
+			'layouts/**/*.vue',
+			'pages/**/*.vue',
+			'plugins/**/*.js',
+			'nuxt.config.js',
+			// TypeScript
+			'plugins/**/*.ts',
+			'nuxt.config.ts',
+		],
+	},
+};
