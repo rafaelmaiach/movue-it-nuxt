bac094ed731372d42fe35e70261253159b1ce722
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-02-23 22:21:41 -0300

    chore: add static files

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ export default {
 			{ hid: 'description', name: 'description', content: '' },
 		],
 		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+			{ rel: 'icon', type: 'image/png', href: '/favicon.png' },
 			{ rel: 'preconnect', href: 'https://fonts.gstatic.com' },
 			{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap' },
 		],
