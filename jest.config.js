f6c2f647c44d2df262cf826c62a1c66704e81608
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-05 20:07:18 -0300

    chore: configure jest to run ts files

--- jest.config.js
+++ jest.config.js
@@ -15,9 +15,13 @@ module.exports = {
 		'^.+\\.js$': 'babel-jest',
 		'.*\\.(vue)$': 'vue-jest',
 	},
-	collectCoverage: true,
+	collectCoverage: false,
 	collectCoverageFrom: [
 		'<rootDir>/components/**/*.vue',
 		'<rootDir>/pages/**/*.vue',
+		'<rootDir>/layouts/**/*.vue',
+		'<rootDir>/store/**/*.ts',
+		'<rootDir>/utils/**/*.ts',
+		'!<rootDir>/**/types.ts',
 	],
 };
