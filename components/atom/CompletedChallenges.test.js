d5bc1d7816bee705593cbeeca0f7de3cb0d30422
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-06 17:25:07 -0300

    test: completed challenges atom unit test

---
+++ components/atom/CompletedChallenges.test.js
@@ -0,0 +1,33 @@
+import { mount } from '@vue/test-utils';
+
+import { setupLocalVueStore } from '~/store/helper';
+
+import CompletedChallenges from './CompletedChallenges.vue';
+
+describe('Components:atom:CompletedChallenges', () => {
+	const { localVue, configureStore } = setupLocalVueStore();
+	let mountConfig;
+
+	beforeEach(() => {
+		mountConfig = {
+			localVue,
+		};
+	});
+
+	const buildWrapper = () => {
+		const store = configureStore();
+		return {
+			...mountConfig,
+			store,
+		};
+	};
+
+	describe('Snapshots', () => {
+		it('should match snapshot', () => {
+			const config = buildWrapper();
+			const wrapper = mount(CompletedChallenges, config);
+
+			expect(wrapper.html()).toMatchSnapshot();
+		});
+	});
+});
