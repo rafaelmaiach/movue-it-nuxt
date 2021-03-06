92bd308aee970474bf76f9f2d824864d80596504
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-06 17:39:49 -0300

    test: level up modal atom unit test

---
+++ components/atom/LevelUpModal.test.js
@@ -0,0 +1,62 @@
+import { mount } from '@vue/test-utils';
+
+import { setupLocalVueStore } from '~/store/helper';
+
+import LevelUpModal from './LevelUpModal.vue';
+
+describe('Components:atom:LevelUpModal', () => {
+	const { localVue, configureStore } = setupLocalVueStore();
+	let mountConfig;
+
+	beforeEach(() => {
+		mountConfig = {
+			localVue,
+		};
+	});
+
+	const buildWrapper = (challengesState = {}) => {
+		const store = configureStore({}, challengesState);
+		return {
+			...mountConfig,
+			store,
+		};
+	};
+
+	describe('Snapshots', () => {
+		it('should match snapshot when modal is opened', () => {
+			const config = buildWrapper({ isLevelUpModalOpen: true });
+			const wrapper = mount(LevelUpModal, config);
+
+			expect(wrapper.html()).toMatchSnapshot();
+		});
+
+		it('should match snapshot when modal is closed', () => {
+			const config = buildWrapper({ isLevelUpModalOpen: false });
+			const wrapper = mount(LevelUpModal, config);
+
+			expect(wrapper.html()).toMatchSnapshot();
+		});
+	});
+
+	describe('Behavior', () => {
+		it('should close modal when overlay is clicked', async () => {
+			const config = buildWrapper({ isLevelUpModalOpen: true });
+			const wrapper = mount(LevelUpModal, config);
+			const overlay = wrapper.find('.overlay');
+
+			await overlay.trigger('click');
+
+			expect(wrapper.find('.overlay').exists()).toBe(false);
+		});
+
+		it('should close modal when close button is clicked', async () => {
+			const config = buildWrapper({ isLevelUpModalOpen: true });
+			const wrapper = mount(LevelUpModal, config);
+			const button = wrapper.find('button');
+
+			await button.trigger('click');
+
+			expect(wrapper.find('.overlay').exists()).toBe(false);
+		});
+	});
+});
