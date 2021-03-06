d9efe7da3e61a6bb1651f4d68966c8e2ce156cad
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-06 17:29:51 -0300

    test: experience bar atom unit test

---
+++ components/atom/ExperienceBar.test.js
@@ -0,0 +1,20 @@
+import { mount } from '@vue/test-utils';
+
+import { setupLocalVueStore } from '~/store/helper';
+
+import ExperienceBar from './ExperienceBar.vue';
+
+describe('Components:atom:ExperienceBar', () => {
+	const { localVue, configureStore } = setupLocalVueStore();
+
+	describe('Snapshots', () => {
+		it('should match snapshot', () => {
+			const wrapper = mount(ExperienceBar, {
+				localVue,
+				store: configureStore(),
+			});
+
+			expect(wrapper.html()).toMatchSnapshot();
+		});
+	});
+});
