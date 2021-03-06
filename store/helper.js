12876c4e4567a798a22e3b61c6916b45ba9c90d2
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-06 00:01:24 -0300

    feat: creates test helper to create store

---
+++ store/helper.js
@@ -0,0 +1,55 @@
+import { createLocalVue } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import * as Challenges from '~/store/Challenges';
+import * as Countdown from '~/store/Countdown';
+
+export const setupLocalVueStore = () => {
+	const localVue = createLocalVue();
+	localVue.use(Vuex);
+
+	const setupModule = () => ({
+		modules: {
+			Countdown: {
+				namespaced: true,
+				state: Countdown.state(),
+				getters: Countdown.getters,
+				mutations: Countdown.mutations,
+			},
+			Challenges: {
+				namespaced: true,
+				state: {
+					...Challenges.state(),
+					allChallenges: [{
+						type: 'type',
+						description: 'description',
+						amount: 10,
+					}],
+				},
+				getters: Challenges.getters,
+				mutations: Challenges.mutations,
+			},
+		},
+	});
+
+	const configureStore = (countdownState = {}, challengesState = {}) => {
+		const storeConfig = setupModule();
+		storeConfig.modules.Countdown.state = {
+			...storeConfig.modules.Countdown.state,
+			...countdownState,
+		};
+
+		storeConfig.modules.Challenges.state = {
+			...storeConfig.modules.Challenges.state,
+			...challengesState,
+		};
+
+		const store = new Vuex.Store(storeConfig);
+		return store;
+	};
+
+	return {
+		localVue,
+		configureStore,
+	};
+};
