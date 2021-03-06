3e32899d6c95395096534720013038ae97985d60
rafaelmaiach <rafael.chieregatto@hotmail.com>
2021-03-06 17:27:43 -0300

    test: countdown digits atom unit test

---
+++ components/atom/CountdownDigits.test.js
@@ -0,0 +1,17 @@
+import { mount } from '@vue/test-utils';
+
+import CountdownDigits from './CountdownDigits.vue';
+
+describe('Components:atom:CountdownDigits', () => {
+	describe('Snapshots', () => {
+		it('should match snapshot', () => {
+			const wrapper = mount(CountdownDigits, {
+				propsData: {
+					digits: 10,
+				},
+			});
+
+			expect(wrapper.html()).toMatchSnapshot();
+		});
+	});
+});
