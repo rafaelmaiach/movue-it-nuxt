52cffe1a69b3958d5ddfeb7b2b503cc28950a5e0
Abimael Tavares <49216154+abimaelst@users.noreply.github.com>
2021-07-05 19:09:51 -0300

    new add variables  to template
    add new variables after change props types to template

--- components/molecules/Challenge.vue
+++ components/molecules/Challenge.vue
@@ -1,28 +1,30 @@
 <template>
-	<section class="flex flex-col flex-1 justify-center items-center py-6 px-10 md:px-16">
-		<header class="w-full pb-6 border-b-2 border-background">
-			<h2 class="text-xl text-blue font-semibold text-center">
-				{{ `Win ${amount} xp` }}
-			</h2>
-		</header>
-		<main class="flex flex-col flex-1 justify-center items-center mt-6">
-			<img :src="`icons/${type}.svg`" :alt="type" :style="{ minHeight: '70px' }">
-			<h1 class="font-semibold text-3xl text-title mt-6 mb-2">
-				Work out
-			</h1>
-			<p class="text-center text-base leading-6">
-				{{ description }}
-			</p>
-		</main>
-		<footer class="flex w-full gap-x-2">
-			<button class="button failed" @click="resetChallenges">
-				Failed
-			</button>
-			<button class="button succeeded" @click="challengeSucceeded">
-				Completed
-			</button>
-		</footer>
-	</section>
+  <section
+    class="flex flex-col flex-1 justify-center items-center py-6 px-10 md:px-16"
+  >
+    <header class="w-full pb-6 border-b-2 border-background">
+      <h2 class="text-xl text-blue font-semibold text-center">
+        {{ `Win ${challenge.amount} xp` }}
+      </h2>
+    </header>
+    <main class="flex flex-col flex-1 justify-center items-center mt-6">
+      <img
+        :src="`icons/${challenge.type}.svg`"
+        :alt="challenge.type"
+        :style="{ minHeight: '70px' }"
+      />
+      <h1 class="font-semibold text-3xl text-title mt-6 mb-2">Work out</h1>
+      <p class="text-center text-base leading-6">
+        {{ challenge.description }}
+      </p>
+    </main>
+    <footer class="flex w-full gap-x-2">
+      <button class="button failed" @click="resetChallenges">Failed</button>
+      <button class="button succeeded" @click="challengeSucceeded">
+        Completed
+      </button>
+    </footer>
+  </section>
 </template>
 
 <script lang="ts">
